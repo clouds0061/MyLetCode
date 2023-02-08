e15e1d4ecc078ca0b77df3099d624aab262dd9a3
mj <1533836992@qq.com>
2023-02-08 20:47:30 +0800

    1.app启动白屏和黑屏处理，添加SplashActivity

---
+++ app/src/main/java/com/example/MainActivity2.kt
@@ -0,0 +1,20 @@
+package com.example
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myletcode.R
+import com.example.myletcode.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    lateinit var binding  : ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
